06629f0e1133a7f8dbfbc55fc840ff782f8105d5
DeviousLab <deviouslab@gmail.com>
2021-09-07 00:00:59 +0400

    Added

---
+++ JavaScript/Circles/app.js
@@ -0,0 +1,3 @@
+const redCircle = document.querySelector('#red-circle').addEventListener(onclick, () => redCircle.style.display = 'none')
+const blueCircle = document.querySelector('#blue-circle').addEventListener(onclick, () => blueCircle.style.display = 'none')
+const greenCircle = document.querySelector('#green-circle').addEventListener(onclick, () => greenCircle.style.display = 'none')
