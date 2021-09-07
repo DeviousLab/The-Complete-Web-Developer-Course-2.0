14921ad8673cac71a43643256ee6ca9850057c2c
DeviousLab <deviouslab@gmail.com>
2021-09-08 00:09:32 +0400

    Completed reaction tester
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- JavaScript/Reaction Tester/app.js
+++ JavaScript/Reaction Tester/app.js
@@ -6,14 +6,32 @@ shape.addEventListener('click', function() {
     let reactionTime = (endTime - startTime)/1000;
     shape.style.display = 'none';
     document.querySelector('#timeTaken').innerHTML = `${reactionTime} seconds`;
+    makeShapesAppear();
 });
 
-setTimeout(function makeShapesAppear() {
+function makeShapesAppear() {
+    shape.style.display = 'block';
+
     let randomTop = Math.random() * 400;
     shape.style.top = `${randomTop}px`;
+
     let randomLeft = Math.random() * 400;
     shape.style.left = `${randomLeft}px`;
-    shape.style.display = 'block';
-    startTime = new Date().getTime();
-},Math.random()*2000);
 
+    let size = (Math.random() * 300)+100;
+    shape.style.width = `${size}px`;
+    shape.style.height = `${size}px`;
+
+    let randomColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    shape.style.backgroundColor = randomColor;
+
+    if (Math.random() > 0.5) {
+        shape.style.borderRadius = '50%';
+    } else {
+        shape.style.borderRadius = '0';
+    };
+
+    startTime = new Date().getTime();
+}
+
+setTimeout(makeShapesAppear, Math.random()*3000);
